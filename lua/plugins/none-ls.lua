fb2e2be0ed567ad141c15fe92f01ac9fa4a26de9
wellington.avelinodo <wellington.avelinodo@servicenow.com>
2024-06-06 10:26:04 +0200

    feat: Update config to use telescope and live grep

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,21 @@
 return {
 	"nvimtools/none-ls.nvim",
-	config = function()
+	 dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+
+  config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
+        require("none-ls.diagnostics.eslint"),
+        null_ls.builtins.completion.luasnip ,
+        null_ls.builtins.code_actions.refactoring,
 			},
 		})
 
