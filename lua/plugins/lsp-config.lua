fb1cc46bc4d3226b3e1ba682ab2b222e22801fe6
wellavelino <wellington.avelino@outlook.com>
2024-06-14 10:10:45 +0200

    feat: add formatter and keymaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,85 +1,105 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "cssls", "html", "htmx", "jsonls", "tsserver", "yamlls", "dockerls", "eslint",
-        "elixirls", "gopls", "graphql", "pylsp", "rust_analyzer"}
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"htmx",
+					"jsonls",
+					"tsserver",
+					"yamlls",
+					"dockerls",
+					"eslint",
+					"elixirls",
+					"gopls",
+					"graphql",
+					"pylsp",
+					"rust_analyzer",
+					"lua_ls",
+					"markdown_oxide",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				autostart = true,
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+					},
+				},
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.cssls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.jsonls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.yamlls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "EslintFixAll",
+					})
+				end,
+			})
 
-      lspconfig.dockerls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.elixirls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.eslint.setup({
-        capabilities = capabilities
-      })
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.elixirls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.graphql.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.gopls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.graphql.setup({
-        capabilities = capabilities
-      })
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.pylsp.setup({
-        capabilities = capabilities
-      })
+			lspconfig.htmx.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities
-      })
-
-      lspconfig.htmx.setup({
-        capabilities = capabilities
-      })
-
-    end,
-  },
+			lspconfig.markdown_oxide.setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
